8e7b11336886b7c35f3dfc5b818b8fa96a4cc91d
Tarek Bashir <tarekb93@gmail.com>
2020-10-01 04:38:12 +0300

    adding auto complete still needs css

---
+++ src/pages/component/autoComplete.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+// import Worker from "./Profission";
+// import TextField from '@material-ui/core/TextField';
+// import Autocomplete from '@material-ui/lab/Autocomplete';
+// import home from "../home";
+
+
+// export default function ComboBox() {
+//   return (
+//     <Autocomplete
+//       id="combo-box-demo"
+//       options={profession.name}
+//       getOptionLabel={(option) => option.title}
+//       style={{ width: 300 }}
+//       renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+//     />
+//   );
+// }
+
+
+
+
+
+// }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Profission from "./component/Profission";
 import "./home.css";
 import { getData, API_BASE } from "../utils/data_helpers";
 import { useHistory } from "react-router-dom";
+// import ComboBox from "./component/autoComplete";
 
 const Home = function () {
-  const [professions, setProfessions] = React.useState([{ name: "Electric" }]);
+  const [professions, setProfessions] = useState([
+    { name: "Electric", identifyLet: "" },
+  ]);
+  const [display, setDisplay] = useState(false);
+  const [search, setSearch] = useState("");
+  const wrapperRef = useRef(null);
 
   const history = useHistory();
   const handleClick = (profession) => {
@@ -14,15 +20,36 @@ const Home = function () {
     history.push("/workerList");
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     const url = `/professions`;
 
     getData(url).then(({ data }) => setProfessions(data));
   }, []);
 
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutSide);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutSide);
+    };
+  }, []);
+
   if (!professions) {
     return <h3>...Loading</h3>;
   }
+  // localStorage.setItem("profs", professions);
+
+  const handleClickOutSide = (event) => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const setProfeDex = (profe) => {
+    setSearch(profe);
+    setDisplay(false);
+  };
 
   return (
     <div className="allH">
@@ -36,14 +63,35 @@ const Home = function () {
 
         {/* in order to have the search icon just like in the design we need a wrapper 
         element (the div) to be relatic and the icon absolute */}
-        <div className="icon-input-wrapper">
+        <div className="icon-input-wrapper" ref={wrapperRef}>
           <img src="/search.svg" className="input-icon" alt="" />
           <input
             type="search"
             name=""
             placeholder="Search for Professional Worker"
             className="searchProfessional"
+            onClick={() => setDisplay(!display)}
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
           />
+          {display && (
+            <div className="autoContainer">
+              {professions
+                .filter(({ name }) => name.indexOf(search.toLowerCase()) > -1)
+                .map((profession) => {
+                  return (
+                    <div
+                      className="option"
+                      tabIndex="0"
+                      onClick={() => setProfeDex(profession.name)}
+                      //onClick = {handleClick}?
+                    >
+                      <span> {profession.name}</span>
+                    </div>
+                  );
+                })}
+            </div>
+          )}
         </div>
         {professions.map((profession) => (
           <Profission
